effd23b599384809fecd0c21dc601db5467414e5
Your Name <you@example.com>
2025-05-09 13:19:22 -0500

    m

--- Calculadora-Modelar/Operaciones.py
+++ Calculadora-Modelar/Operaciones.py
@@ -6,3 +6,17 @@ def resta(a, b):
 
 def multiplicacion(a, b):
     return a * b
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero"
+
+def potencia(a, b):
+    return a ** b
+
+def division_entera(a, b):
+    if b != 0:
+        return a // b
+    else:
+        return "Error: División por cero"
